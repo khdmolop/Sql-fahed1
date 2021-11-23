06550b592f1ba44afc364fc6dddbc52ea20e3b5a
khdmolop <83680629+khdmolop@users.noreply.github.com>
2021-11-23 21:42:39 +0100

    Add files via upload

---
+++ while loop sql.sql
@@ -0,0 +1,23 @@
+DECLARE @X INT
+SET @X= 0
+
+WHILE @X < 5
+BEGIN
+PRINT'KHALED'
+SET @X = @X+1
+END
+
+-------------------------------------
+
+DECLARE @X INT
+SET @X= 0
+
+WHILE @X < 5
+BEGIN
+PRINT'KHALED'
+print @x
+SET @X = @X+1
+
+
+--------------------------------------------
+
